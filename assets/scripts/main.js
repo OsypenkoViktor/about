466cfae93955d2c37fbcd7889f0c2bd00c4deb0e
OsypenkoViktor <143928700+OsypenkoViktor@users.noreply.github.com>
2023-11-09 00:42:28 +0200

    minor fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -15,31 +15,11 @@ const body = document.querySelector("body");
 const introCard = document.querySelector(".intro");
 const skillsTable = document.querySelector(".skillTable");
 
-// Список фоновых изображений для предварительной загрузки
-const backgroundImages = ["assets/images/introBG.jpg", "assets/images/skillsTableBG.jpg"];
-const preloadedImages = [];
 
-// Функция для предварительной загрузки изображений
-function preloadImages(images) {
-  images.forEach((src) => {
-    const img = new Image();
-    img.src = src;
-    img.onload = () => {
-      preloadedImages.push(src);
-      // Проверить, что все изображения загружены
-      if (preloadedImages.length === images.length) {
+
+
         window.addEventListener("scroll", () => {
-          if (isElementInViewport(introCard) && preloadedImages.includes("assets/images/introBG.jpg")) {
-            body.style.backgroundImage = "url('assets/images/introBG.jpg')";
-          }
-          if (isElementInViewport(skillsTable) && preloadedImages.includes("assets/images/skillsTableBG.jpg")) {
-            body.style.backgroundImage = "url('assets/images/skillsTableBG.jpg')";
-          }
-        });
-      }
-    };
-  });
-}
-
-// Предварительно загрузить изображения
-preloadImages(backgroundImages);
+          isElementInViewport(introCard) &&(body.style.backgroundImage = "url('assets/images/introBG.jpg')")
+          isElementInViewport(skillsTable) && (body.style.backgroundImage = "url('assets/images/skillsTableBG.jpg')")
+      });
+      
