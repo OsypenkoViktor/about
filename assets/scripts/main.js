50bdad004fc31d9cc98875fc3f1c7699025e6116
OsypenkoViktor <143928700+OsypenkoViktor@users.noreply.github.com>
2023-11-09 20:18:48 +0200

    modified:   assets/scripts/main.js 	modified:   assets/styles/style.css 	modified:   index.html -додано динамічний заголовок -імплементовані базові респонсів-стилі

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,11 +12,78 @@ function isElementInViewport(element) {
 }
 
 const body = document.querySelector("body");
-const introCard = document.querySelector(".intro");
+const introCard = document.querySelector(".photo");
 const skillsTable = document.querySelector(".skillTable");
+const dynamicTitle = document.querySelector(".dynamicTitle")
 
-        window.addEventListener("scroll", () => {
-          isElementInViewport(introCard) &&(body.style.setProperty('--bgImage', "url('../images/introBG.jpg')"))
-          isElementInViewport(skillsTable) &&(body.style.setProperty('--bgImage', "url('../images/skillsTableBG.jpg')"))
-      });
+const pageData={
+  introCard:{
+    img:"url('../images/introBG.jpg')",
+    dynamicTitle:"Головна"
+  },
+  skillsTable:{
+    img:"url('../images/skillsTableBG.jpg')",
+    dynamicTitle:"Таблиця хард-скілів"
+  }
+}
+
+function findParentIndex(title) {
+  for (const key in pageData) {
+    if (pageData[key].dynamicTitle === title) {
+      return key;
+    }
+  }
+  return -1; // Return -1 if not found
+}
+
+
+let currentElem;
+//функція для оновлення данних документу
+function updatePage(newElem,newBackground,newDTitle){
+  if(newElem==currentElem) return
+  currentElem=newElem
+  body.style.setProperty('--bgImage', newBackground);
+  let oldDTitle = dynamicTitle.innerText;
+  if(!oldDTitle) {
+    dynamicTitle.innerText = newDTitle;
+  }
+  if(oldDTitle){
+    if(findParentIndex(oldDTitle)>findParentIndex(newDTitle)){
+      console.log("up");
+      dynamicTitle.classList.add("fadeDown")
+      setTimeout(()=>{
+        dynamicTitle.innerText = newDTitle;
+        dynamicTitle.classList.add("showUp")
+        dynamicTitle.classList.remove("showUp","fadeDown")
+      },300)
       
+    }
+    if(findParentIndex(oldDTitle)<findParentIndex(newDTitle)){
+      console.log("down");
+      dynamicTitle.classList.add("fadeUp")
+      setTimeout(()=>{
+        dynamicTitle.innerText = newDTitle;
+        dynamicTitle.classList.add("showDown")
+        setTimeout(()=>{
+          dynamicTitle.classList.remove("showDown","fadeUp")
+        },200)
+        
+      },400)
+      
+    }
+    
+  }
+  //dynamicTitle.innerText = newDTitle;
+}
+//функція для оновлення сторінки в залежності від елементів у вьюпорті
+function renderPage(){
+  isElementInViewport(introCard) && 
+  updatePage(introCard,pageData.introCard.img,pageData.introCard.dynamicTitle);
+  isElementInViewport(skillsTable) &&
+  updatePage(skillsTable,pageData.skillsTable.img,pageData.skillsTable.dynamicTitle)
+}
+
+window.addEventListener("scroll", renderPage);
+
+renderPage()
+
